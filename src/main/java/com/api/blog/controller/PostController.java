b16d6995cc8dd72d7875f4b075b8cbdacd29d025
Tomas Perez <tomas.p.r7@gmail.com>
2021-07-20 00:28:43 -0300

    Adding endpoints in post controller

--- src/main/java/com/api/blog/controller/PostController.java
+++ src/main/java/com/api/blog/controller/PostController.java
@@ -1,9 +1,48 @@
 package com.api.blog.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.blog.dto.PostDetailsDto;
+import com.api.blog.model.Post;
+import com.api.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+
+    @Autowired
+    PostService postService;
+
+    @GetMapping
+    public List<PostDetailsDto>  getPostsDetails(){
+        List<Post> posts = postService.findAllPostsDetailsOrdByDate();
+        List<PostDetailsDto> postsDto = posts.stream()
+                .map(post -> new PostDetailsDto(
+                        post.getId(),
+                        post.getTitulo(),
+                        post.getImagen(),
+                        post.getCategoria().getNombre(),
+                        post.getFechaCreacion()
+                )).collect(Collectors.toList());
+        return postsDto;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getPostById(@PathVariable Long id){
+        Optional<Post> post = postService.findPostById(id);
+        return post.isEmpty() ?
+                ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado") :
+                ResponseEntity.accepted().body(post.get());
+    }
+
+    @PostMapping
+    public void savePost(@RequestBody Post post){
+        postService.savePost(post);
+    }
 }
