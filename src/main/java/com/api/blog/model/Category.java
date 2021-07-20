2594c9c6ed6d2c96c0f991d07a303712cf6b01fd
Tomas Perez <tomas.p.r7@gmail.com>
2021-07-20 15:02:19 -0300

    Added annotations to category

--- src/main/java/com/api/blog/model/Category.java
+++ src/main/java/com/api/blog/model/Category.java
@@ -1,14 +1,18 @@
 package com.api.blog.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
 }
