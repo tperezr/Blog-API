78de8659b42bdcca178c5a612ca365327d902ebf
Tomas Perez <tomas.p.r7@gmail.com>
2021-07-20 11:28:27 -0300

    Adding annotations for soft delete

--- src/main/java/com/api/blog/model/Post.java
+++ src/main/java/com/api/blog/model/Post.java
@@ -1,14 +1,19 @@
 package com.api.blog.model;
 
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.util.Date;
 
 @Entity
 @Data
+@SQLDelete(sql = "update post set is_deleted = 1 where id =?")
+@Where(clause = "isDeleted = 0")
 public class Post {
     @Id
     private Long id;
@@ -16,10 +21,13 @@ public class Post {
     private String contenido;
     private String imagen;
     private Date fechaCreacion;
+    private boolean isDeleted = Boolean.FALSE;
 
     @ManyToOne
+    @JoinColumn(name = "id_categoria")
     private Category categoria;
 
     @ManyToOne
+    @JoinColumn(name = "id_usuario")
     private User usuario;
 }
