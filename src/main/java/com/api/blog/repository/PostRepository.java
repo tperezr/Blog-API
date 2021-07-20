638e360933f4225affc0fde7dd7eccc2ccc40606
Tomas Perez <tomas.p.r7@gmail.com>
2021-07-20 15:04:47 -0300

    Added methods to get data from the db

--- src/main/java/com/api/blog/repository/PostRepository.java
+++ src/main/java/com/api/blog/repository/PostRepository.java
@@ -1,9 +1,15 @@
 package com.api.blog.repository;
 
+import com.api.blog.model.Category;
 import com.api.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findAllByTituloAndCategoria(String title, Category category);
+    List<Post> findAllByTitulo(String title);
+    List<Post> findAllByCategoria(Category category);
 }
