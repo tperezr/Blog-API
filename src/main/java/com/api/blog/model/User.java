2dd36bb7cff8f3f707e0d26fbf1be07813a7943b
Tomas Perez <tomas.p.r7@gmail.com>
2021-07-20 00:29:38 -0300

    Adding annotations to user

--- src/main/java/com/api/blog/model/User.java
+++ src/main/java/com/api/blog/model/User.java
@@ -1,12 +1,16 @@
 package com.api.blog.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     private Long id;
