91c5266c35f8bdbf02768edd64314b933e4fd9f0
Tomas Perez <tomas.p.r7@gmail.com>
2021-07-20 15:04:12 -0300

    Added all endpoints and functionality

--- src/main/java/com/api/blog/controller/PostController.java
+++ src/main/java/com/api/blog/controller/PostController.java
@@ -1,11 +1,14 @@
 package com.api.blog.controller;
 
 import com.api.blog.dto.PostDetailsDto;
+import com.api.blog.dto.PostDto;
 import com.api.blog.model.Post;
 import com.api.blog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,8 +23,98 @@ public class PostController {
     PostService postService;
 
     @GetMapping
-    public List<PostDetailsDto>  getPostsDetails(){
+    public List<PostDetailsDto> getPostsDetails(){
         List<Post> posts = postService.findAllPostsDetailsOrdByDate();
+        List<PostDetailsDto> postsDto = createPostsDetailsDto(posts);
+        return postsDto;
+    }
+
+    @GetMapping(params = "title")
+    public ResponseEntity getPostsByTitle(@RequestParam String title){
+        if(title.isBlank()){
+            return ResponseEntity.badRequest().body("Title can not be empty");
+        }
+        List<Post> posts = postService.findAllPostsByTitle(title);
+        List<PostDetailsDto> postsDto = createPostsDetailsDto(posts);
+        return ResponseEntity.ok(postsDto);
+    }
+
+    @GetMapping(params = "category")
+    public ResponseEntity getPostsByCategory(@RequestParam String category){
+        if(category.isBlank()){
+            return ResponseEntity.badRequest().body("Category can not be empty");
+        }
+        List<Post> posts;
+        List<PostDetailsDto> postsDto;
+        try{
+            posts = postService.findAllPostsByCategory(category);
+            postsDto = createPostsDetailsDto(posts);
+        } catch(IllegalArgumentException e){
+            return ResponseEntity.badRequest().body("Invalid category");
+        }
+
+        return ResponseEntity.ok(postsDto);
+    }
+
+    @GetMapping(params = {"title","category"})
+    public ResponseEntity getPostsByCategory(@RequestParam String title, @RequestParam String category){
+        if(title.isBlank() || category.isBlank()){
+            return ResponseEntity.badRequest().body("Category and title can not be empty");
+        }
+        List<Post> posts;
+        List<PostDetailsDto> postsDto;
+        try{
+            posts = postService.findAllPostsByTitleAndCategory(title,category);
+            postsDto = createPostsDetailsDto(posts);
+        } catch(IllegalArgumentException e){
+            return ResponseEntity.badRequest().body("Invalid category");
+        }
+        return ResponseEntity.ok(postsDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getPostById(@PathVariable Long id){
+        Optional<Post> post = postService.findPostById(id);
+        return post.isEmpty() ?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).body(null) :
+                ResponseEntity.accepted().body(createPostDto(post.get()));
+    }
+
+    @PostMapping
+    public ResponseEntity savePost(@RequestBody Post post){
+        postService.savePost(post);
+        return ResponseEntity.ok("Successfully saved");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletePost(@PathVariable Long id){
+        try{
+            postService.deletePost(id);
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok("Deleted");
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity updatePost(){
+        return null;
+    }
+
+    private PostDto createPostDto(Post post){
+        PostDto postDto = new PostDto(
+                post.getId(),
+                post.getTitulo(),
+                post.getContenido(),
+                post.getImagen(),
+                post.getFechaCreacion(),
+                post.getCategoria().getNombre(),
+                post.getUsuario().getEmail()
+        );
+        return postDto;
+    }
+
+    private List<PostDetailsDto> createPostsDetailsDto(List<Post> posts){
         List<PostDetailsDto> postsDto = posts.stream()
                 .map(post -> new PostDetailsDto(
                         post.getId(),
@@ -31,18 +124,5 @@ public class PostController {
                         post.getFechaCreacion()
                 )).collect(Collectors.toList());
         return postsDto;
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getPostById(@PathVariable Long id){
-        Optional<Post> post = postService.findPostById(id);
-        return post.isEmpty() ?
-                ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado") :
-                ResponseEntity.accepted().body(post.get());
-    }
-
-    @PostMapping
-    public void savePost(@RequestBody Post post){
-        postService.savePost(post);
     }
 }
