e27205ad436d837062067af27dc2f26a6df9e3f8
Tomas Perez <tomas.p.r7@gmail.com>
2021-07-21 16:51:28 -0300

    Entry point created for exception handling

--- src/main/java/com/api/blog/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/api/blog/config/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,19 @@
 package com.api.blog.config;
 
-public class JwtAuthenticationEntryPoint {
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Missing or bad authentication");
+    }
 }
