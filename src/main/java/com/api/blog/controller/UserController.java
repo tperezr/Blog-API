138997a921373aef563c51c32d13f1c0da323044
Tomas Perez <tomas.p.r7@gmail.com>
2021-07-20 21:55:50 -0300

    Sing up and login endpoints added

--- src/main/java/com/api/blog/controller/UserController.java
+++ src/main/java/com/api/blog/controller/UserController.java
@@ -1,6 +1,16 @@
 package com.api.blog.controller;
 
+import com.api.blog.dto.UserDto;
 import com.api.blog.model.User;
+import com.api.blog.service.UserService;
+import com.api.blog.util.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +20,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
     @PostMapping("/login")
-    public String login(@RequestBody User user){
-        return "";
+    public ResponseEntity login(@RequestBody UserDto userLogin){
+        try {
+            authenticate(userLogin.getEmail(),userLogin.getPassword());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+        UserDetails userDetails = userService.loadUserByUsername(userLogin.getEmail());
+
+        String token = jwtTokenUtil.getToken(userDetails);
+
+        return ResponseEntity.ok(token);
     }
 
-    @PostMapping
-    public void singUp(@RequestBody User user){
+    @PostMapping("/sign_up")
+    public ResponseEntity singUp(@RequestBody UserDto userSignUp){
+        if(userService.signUpUser(userSignUp)) {
+            return ResponseEntity.ok("Registro exitoso");
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Usuario ya existente");
+    }
 
+    private void authenticate(String email, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        } catch (BadCredentialsException e) {
+            throw new Exception("Credenciales invalidas", e);
+        }
     }
 }
