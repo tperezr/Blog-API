69e94b81762a1a407c7a47024d01959d3039a55c
Tomas Perez <tomas.p.r7@gmail.com>
2021-07-22 13:32:40 -0300

    Exception handler for validation in requests designed.

---
+++ src/main/java/com/api/blog/controller/RestResponseExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.api.blog.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+
+@ControllerAdvice
+public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request){
+        int index = ex.getMessage().lastIndexOf('.');
+        String message = ex.getMessage().substring(index+1);
+        return handleExceptionInternal(ex, message, new HttpHeaders(),HttpStatus.BAD_REQUEST,request);
+    }
+}
