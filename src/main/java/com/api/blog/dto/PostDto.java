750b19516667ce3b7bd1b17fb5579b13d7d1a808
Tomas Perez <tomas.p.r7@gmail.com>
2021-07-23 12:17:05 -0300

    Annotations to valid fields added.

--- src/main/java/com/api/blog/dto/PostDto.java
+++ src/main/java/com/api/blog/dto/PostDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
@@ -11,10 +12,16 @@ import java.util.Date;
 @NoArgsConstructor
 public class PostDto {
     private Long id;
+    @NotNull
     private String titulo;
+    @NotNull
     private String contenido;
+    @NotNull
     private String imagen;
+    @NotNull
     private Date fechaCreacion;
+    @NotNull
     private String categoria;
+    @NotNull
     private String usuario;
 }
