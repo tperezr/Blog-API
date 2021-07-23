4f48dba252e7c64c554a3ff8648ba0ee00cd6399
Tomas Perez <tomas.p.r7@gmail.com>
2021-07-23 12:15:43 -0300

    Annotations to valid fields added

--- src/main/java/com/api/blog/dto/UserDto.java
+++ src/main/java/com/api/blog/dto/UserDto.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
+    @NotNull
     private String email;
+    @NotNull
     private String password;
 }
